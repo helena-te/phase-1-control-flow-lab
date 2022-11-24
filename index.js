eb5f3a2a2cef4a1c870a38b38b1e4b194191cc00
Helena Tenev <htenev14@gmail.com>
2022-11-23 22:19:05 -0500

    commit tree 7c40ec2b528051353c24e9b41528721e7baa48e3

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number){
   // Write your code here!
+  if (number <= 400) {
+   return('This one is on me!');
+  }
+  else if (number > 400 && number <=2000) {
+   return('That will be twenty bucks.');
+  }
+  else if (number > 2000 && number <=2500) {
+    return('I will gladly take your thirty bucks.');
+  }
+  else {
+    return('No can do.');
+  }
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+  const msg = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return msg
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  let charm;
+  switch (tip) {
+    case "generous":
+      charm= "Thank you so much.";
+      break;
+    case 'not as generous':
+      charm = "Thank you."
+      break;
+    default: 
+      charm = "Bye."
+      break;
+      } 
+    return charm;
 }
